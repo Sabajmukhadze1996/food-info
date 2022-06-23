72f3db77621bc2603b5615d7227bdd54db8b5862
Sabajmukhadze1996 <sabajmukhadze23@gmail.com>
2022-06-24 01:13:12 +0400

    updated

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "../node_modules/bootstrap/dist/js/bootstrap.min.js"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
